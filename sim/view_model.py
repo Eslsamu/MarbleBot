87efe331b855fdd152a5f9357978433cbbb19374
Eslsamu <samuel.kopp@student.maastrichtuniversity.nl>
2019-02-13 11:25:15 +0100

    displays model via command line argument

---
+++ sim/view_model.py
@@ -0,0 +1,15 @@
+import mujoco_py as mjc
+import sys
+import numpy as np
+
+print(sys.argv[1])
+model = mjc.load_model_from_path(sys.argv[1])
+sim = mjc.MjSim(model)
+act_shape = sim.data.ctrl.shape
+state_shape = np.concatenate([
+       sim.data.qpos.flat,
+       sim.data.qvel.flat]).shape
+viewer = mjc.MjViewer(sim)
+
+while True:
+       viewer.render()
