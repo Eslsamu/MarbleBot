8fef57abff84fe65d5c648e8cac872a7e68b649a
Samuel <samuel.r.kopp@gmail.com>
2019-06-17 11:23:28 +0200

    time test

--- sim/webots/controllers/RL_controller/wbt_jobs.py
+++ sim/webots/controllers/RL_controller/wbt_jobs.py
@@ -84,5 +84,5 @@ def run_job(n_proc = 2, n_it = 10, n_steps = 10, build_files = False, data_dir =
 
 
 t = time.time()
-run_job(n_proc = 1, n_it = 160, n_steps= 500, build_files=True)
+run_job(n_proc = 1, n_it = 192, n_steps= 500, build_files=True)
 print("time:", time.time()-t)
