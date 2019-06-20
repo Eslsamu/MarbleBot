212cf4d83003575c3eeb1cd3a6297cbe22b6c51e
Johnson <7kaspu@gmail.com>
2019-06-20 16:26:25 +0200

    imu and energy

--- sim/webots/controllers/RL_controller/robot_environment.py
+++ sim/webots/controllers/RL_controller/robot_environment.py
@@ -1,7 +1,7 @@
 import numpy as np
 import json
 import logging
-
+logging.basicConfig(filename='env.log', format='%(asctime)s %(message)s')
 
 TIMESTEP = 32
 DIRECTION = 0
@@ -14,43 +14,46 @@ class Robot_Environment():
         self.sv = supervisor
         with open(file) as f:
             devices = json.load(f)
-            sensor_names = devices["sensors"]
-            lin_motor_names = devices["lin_motors"]
-            rot_motor_names = devices["rot_motors"]
+            sensor_names = devices["force_sensors"]
+            motor_names = devices["motors"]
+            collision_detector_names = devices["collision_detectors"]
+            IMU_names = devices["IMUs"]
 
         self.init_sensors(sensor_names)
-        self.init_motors(lin_motor_names, rot_motor_names)
+        self.init_motors(motor_names)
+        self.init_collision_detectors(collision_detector_names)
         self.trans_field = self.sv.getFromDef("robot").getField("translation")
-
-        # small float for handling motor velocity limit
-        self.e = 1e-5
 
     def init_sensors(self, sensor_names):
         self.sensors = []
-        for n in sensor_names:
+        for n in force_sensor_names:
             #TODO IMU
             s = self.sv.getTouchSensor(n)
-            s.enable(TIMESTEP*2)
+            s.enable(2*TIMESTEP) # 2 * TIMESTEP  is twice as good.
             self.sensors.append(s)
+        for n in IMU_names:
+            y =  self.sv.getInertialUnit(n)
+            y.enable(2*TIMESTEP)
+            y.sensors.append(y)
 
-    def init_motors(self, lin_motor_names, rot_motor_names):
-        #linear motors
-        self.lin_motors = []
-        for n in lin_motor_names:
+    def init_motors(self, motor_names):
+        # motors
+        self.motors = []
+        for n in motor_names:
             m = self.sv.getMotor(n)
             m.setPosition(float(np.inf))
-            self.lin_motors.append(m)
+            self.motors.append(m)
 
-        #rotational motors
-        self.rot_motors = []
-        for n in rot_motor_names:
-            m = self.sv.getMotor(n)
-            m.setPosition(float(np.inf))
-            self.rot_motors.append(m)
+    def init_collision_detectors(self, collision_detector_names):
+        self.collision_detectors = []
+        for n in collision_detector_names:
+           t = self.sv.getToucSensor(n)
+           t.enable(TIMESTEP)
+           self.collision_detectors.append(t)
+
 
         #TODO different maxvel for linear and rotational motor
-        self.maxLinVel = self.lin_motors[0].getMaxVelocity()
-        self.maxRotVel = self.rot_motors[0].getMaxVelocity()
+        self.maxVel = self.motors[0].getMaxVelocity()
 
     def distance_travelled(self, pos0, pos1, direction=DIRECTION):
         # travlled euclidean distance
@@ -76,46 +79,40 @@ class Robot_Environment():
 
 
     def actuate_motors(self, vel):
-        n_lin = len(self.lin_motors)
-        n_rot = len(self.rot_motors)
+        for m in range(len(self.motors)):
+            self.motors[m].setVelocity(float(vel[m]))
+    
+    def enableBattery(self):
+        batSample = 500 # battery sampling period
+        self.sv.batterySensorEnable(batSample)
+        #startingPower = robot.batterySensorGetValue() # this one won't work because the first proper battery reading will only appear after the first sampling period amount of time has passed
 
-        #set linear motor velocity
-        for m in range(n_lin):
-            #if np.abs(vel[m]) >= self.maxLinVel:
-                #logging.info("action is clipped")
+    #TODO for every sim. step. So compare change in power from step to step, not from start and current step
+    def calculate_energy(self, lastPower, nowPower):
+        return (lastPower - nowPower)
 
-            #TODO better solution than clipping
-            v = np.clip(vel[m], -self.maxLinVel+self.e, self.maxLinVel-self.e)
-            self.lin_motors[m].setVelocity(float(v))
-
-        #set rotational motor velocity
-        for m in range(n_lin, n_rot):
-            #if np.abs(vel[m]) >= self.maxRotVel:
-             #   logging.info("action is clipped")
-            v = np.clip(vel[m], -self.maxRotVel, self.maxRotVel)
-            self.rot_motors[m].setVelocity(float(v))
-
-    #TODO
-    def calculate_energy(self):
-        return 0
-
-    def calculate_reward(self, pos0, pos1):
-        rew = self.distance_travelled(pos0, pos1) #- energy
+    def calculate_reward(self, pos0, pos1, lastPower, nowPower):
+        # energy has high value 
+        rew = self.distance_travelled(pos0, pos1) - self.calculate_energy(lastPower, nowPower) 
         return rew
 
-
     def check_termination(self):
+        # if body rotation is upside down then terminated
         done = False
+        if (self.collision_detectors[0].getValue() == 1): # If robot's touch sensor is making contact with any solid
+            done = True
         return done
 
 
     def step(self,action, t=TIMESTEP):
         self.actuate_motors(action)
         pos0 = self.trans_field.getSFVec3f()
+        power0 = self.sv.batterySensorGetValue()
         self.sv.step(t)
+        power1 = self.sv.batterySensorGetValue()
         pos1 = self.trans_field.getSFVec3f()
-
-        rew = self.calculate_reward(pos0, pos1)
+        
+        rew = self.calculate_reward(pos0, pos1, power0, power1)
         obs = self.get_sensor_data()
         done = self.check_termination()
 
