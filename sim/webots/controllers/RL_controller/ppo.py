variance and energy fix

sim/webots/controllers/RL_controller/ppo.py
@@ -214,7 +214,7 @@ def run_ppo(epochs=30,epoch_steps = 4000 , max_ep_len=500 ,pi_lr = 3e-4, vf_lr=1
         #update policy
         update_info = update()
 
-        with open("epoch_data/sum_ep"+str(epoch)+".p", "wb") as f:
+        with open("sum_ep"+str(epoch)+".p", "wb") as f:
             pickle.dump([epoch_info, update_info], f)
 
 
@@ -228,4 +228,4 @@ with open(file) as f:
 obs_dim = len(sensor_names)
 act_dim = len(motor_names)
 action_scale = np.array([0.2, 0.2, 0.2, 0.2, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0])
-run_ppo(epochs=100, epoch_steps=4000, act_dim = act_dim, obs_dim = obs_dim, action_scale=action_scale, n_proc=24)
+run_ppo(epochs=100, epoch_steps=4000, act_dim = act_dim, obs_dim = obs_dim, action_scale=action_scale, n_proc=1)

sim/webots/controllers/RL_controller/rl_policy.py
@@ -55,9 +55,8 @@ def policy(state, action, hidden_sizes):
     with tf.name_scope('mean'):
         mean = tf.layers.dense(net, act_dim, activation=None)
 
-    #squash standard deviation to avoid extrem values
     with tf.variable_scope('log_std'):
-        log_std = tf.get_variable(name='log_std', initializer=np.array([0.05,0.05,0.05,0.05,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3], dtype=np.float32))
+        log_std = tf.get_variable(name='log_std', initializer=np.array([-4.5,-4.5,-4.5,-4.5,-2,-2,-2,-2,-2,-2,-2,-2], dtype=np.float32))
         #log_std = tf.layers.dense(net, act_dim, activation=tf.tanh)
         #log_std = LOG_STD_MIN + 0.5 * (LOG_STD_MAX - LOG_STD_MIN) * (log_std + 1)
 